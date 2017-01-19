21a61fb1ff9ccd6946283828a386cac8952e8282
wightman <davidwightmanmusic@gmail.com>
2017-01-19 15:24:57 -0500

    added other users.

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// const user1 = {learn: ['guitar', 'accordian', 'dance'],
-// 	teach: ['tap', 'tap', 'piano']
-// }
-
-// const user2 = {learn: ['tap', 'accordian', 'dance'],
-// 	teach: ['guitar', 'running', 'piano']
-// }
-
 // if (user1.learn[0] === user2.teach[0] && user1.teach[0] === user2.learn[0]) {
 // 	console.log("we have a match")
 // }
@@ -22,8 +14,14 @@ const user1 = {teach: ['guitar', 'accordian', 'dance', 'poetry', 'JavaScript'],
 const user2 = {teach: ['cooking', 'time management', 'painting'],
 	learn: ['piano', 'running', 'JavaScript', 'guitar']}
 
+const user3 = {teach: ['book making', 'cooking', 'public speaking', 'swimming', 'time management'],
+	learn: ['piano', 'running', 'JavaScript', 'guitar']}
 
-function intersect_arrays(a, b) {
+const user4 = {teach: ['JavaScript', 'driving', 'drums'],
+	learn: ['time management', 'bass', 'basic electronics', 'guitar']}
+
+
+function intersect_1teach2(a, b) {
     var sorted_a = a.concat().sort();
     var sorted_b = b.concat().sort();
     var common = [];
@@ -45,11 +43,36 @@ function intersect_arrays(a, b) {
             b_i++;
         }
     }
-    console.log(common.join(", "))
+    console.log("User 1 will be able to teach User 2 " + common.join(", ") + ".")
 }
 
-intersect_arrays(user1.teach, user2.learn) 
-intersect_arrays(user2.teach, user1.learn)
+function intersect_2teach1(a, b) {
+    var sorted_a = a.concat().sort();
+    var sorted_b = b.concat().sort();
+    var common = [];
+    var a_i = 0;
+    var b_i = 0;
+
+    while (a_i < a.length
+           && b_i < b.length)
+    {
+        if (sorted_a[a_i] === sorted_b[b_i]) {
+            common.push(sorted_a[a_i]);
+            a_i++;
+            b_i++;
+        }
+        else if(sorted_a[a_i] < sorted_b[b_i]) {
+            a_i++;
+        }
+        else {
+            b_i++;
+        }
+    }
+    console.log("User 2 will be able to teach User 1 " + common.join(", ") + ".")
+}
+
+intersect_1teach2(user1.teach, user2.learn) 
+intersect_2teach1(user2.teach, user1.learn)
 
 
  // if (match1 != null && match2 != null) {
