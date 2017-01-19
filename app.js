5da1a972e60652fe76762c124d711debc0e6e11a
wightman <davidwightmanmusic@gmail.com>
2017-01-19 15:54:51 -0500

    made the code more functional

--- app.js
+++ app.js
@@ -21,8 +21,58 @@ const user4 = {teach: ['JavaScript', 'driving', 'drums'],
 	learn: ['time management', 'bass', 'basic electronics', 'guitar']}
 
 
-function intersect_1teach2(a, b) {
-    var sorted_a = a.concat().sort();
+// function intersect_1teach2(a, b) {
+//     var sorted_a = a.concat().sort();
+//     var sorted_b = b.concat().sort();
+//     var common = [];
+//     var a_i = 0;
+//     var b_i = 0;
+
+//     while (a_i < a.length
+//            && b_i < b.length)
+//     {
+//         if (sorted_a[a_i] === sorted_b[b_i]) {
+//             common.push(sorted_a[a_i]);
+//             a_i++;
+//             b_i++;
+//         }
+//         else if(sorted_a[a_i] < sorted_b[b_i]) {
+//             a_i++;
+//         }
+//         else {
+//             b_i++;
+//         }
+//     }
+//     console.log("User 1 will be able to teach User 2 " + common.join(", ") + ".")
+// }
+
+// function intersect_2teach1(a, b) {
+//     var sorted_a = a.concat().sort();
+//     var sorted_b = b.concat().sort();
+//     var common = [];
+//     var a_i = 0;
+//     var b_i = 0;
+
+//     while (a_i < a.length
+//            && b_i < b.length)
+//     {
+//         if (sorted_a[a_i] === sorted_b[b_i]) {
+//             common.push(sorted_a[a_i]);
+//             a_i++;
+//             b_i++;
+//         }
+//         else if(sorted_a[a_i] < sorted_b[b_i]) {
+//             a_i++;
+//         }
+//         else {
+//             b_i++;
+//         }
+//     }
+//     console.log("User 2 will be able to teach User 1 " + common.join(", ") + ".")
+// }
+
+var intersection = function(a, b) {
+	var sorted_a = a.concat().sort();
     var sorted_b = b.concat().sort();
     var common = [];
     var a_i = 0;
@@ -43,36 +93,17 @@ function intersect_1teach2(a, b) {
             b_i++;
         }
     }
-    console.log("User 1 will be able to teach User 2 " + common.join(", ") + ".")
+    return common.join(", ") 
 }
 
-function intersect_2teach1(a, b) {
-    var sorted_a = a.concat().sort();
-    var sorted_b = b.concat().sort();
-    var common = [];
-    var a_i = 0;
-    var b_i = 0;
+var oneteachtwo = intersection(user1.teach, user2.learn)
+var twoteachone = intersection(user2.teach, user1.learn)
 
-    while (a_i < a.length
-           && b_i < b.length)
-    {
-        if (sorted_a[a_i] === sorted_b[b_i]) {
-            common.push(sorted_a[a_i]);
-            a_i++;
-            b_i++;
-        }
-        else if(sorted_a[a_i] < sorted_b[b_i]) {
-            a_i++;
-        }
-        else {
-            b_i++;
-        }
-    }
-    console.log("User 2 will be able to teach User 1 " + common.join(", ") + ".")
-}
+console.log("User 1 can teach User 2 " + oneteachtwo);
+console.log("User 2 can teach User 1 " + twoteachone);
 
-intersect_1teach2(user1.teach, user2.learn) 
-intersect_2teach1(user2.teach, user1.learn)
+// intersect_1teach2(user1.teach, user2.learn) 
+// intersect_2teach1(user2.teach, user1.learn)
 
 
  // if (match1 != null && match2 != null) {
